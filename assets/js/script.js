2d5d3af3d568888df165887f7c670215c4b0695b
fullStackHero <hello@avui.design>
2024-09-28 19:05:39 +0000

    header/nav and section prep with web components

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,78 @@
-const todoTemplate = document.createElement('template');
-todoTemplate.innerHTML = `
-    <style>
-        h3 {
-            color: green;
-        }
-    </style>
-    <div class="todo-item">
-    <h3>
-        <slot></slot>
-    </h3>
-`;
-
-class TodoItem extends HTMLElement {
-    constructor() {
-        super();
-        const shadow = this.attachShadow({mode: 'open'});
-        shadow.appendChild(todoTemplate.content.cloneNode(true));
-    }
-}
-
-customElements.define('todo-item', TodoItem);
-
+// HEADER NAVIGATION COMPONENT
 const headerNavTemplate = document.createElement('template');
 headerNavTemplate.innerHTML = `
     <style>
-        h4 {
-            color: blue;
+        header {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 2rem;
+            background-color: #000;
+            color: #fff;
+        }
+
+        header a {
+            color: #fff;
+        }
+
+        nav {
+            display: flex;
+            gap: 2rem;
+            justify-content: center;
+            align-items: center;
         }
     </style>
-    <div class="header-nav">
-        <h4>
-            <slot></slot>
-        </h4>
-    </div>
+    <header class="header-nav">
+        <a href="#" class="logo">
+            AV UI
+        </a>
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+        </nav>
+    </header>
 `;
 
-    class HeaderNav extends HTMLElement {
-        constructor() {
-            super();
-            const shadow = this.attachShadow({ mode: 'open' });
-            shadow.appendChild(headerNavTemplate.content.cloneNode(true));
-        }
+class HeaderNav extends HTMLElement {
+    constructor() {
+        super();
+        const shadow = this.attachShadow({ mode: 'open' });
+        shadow.appendChild(headerNavTemplate.content.cloneNode(true));
     }
+}
 
-    customElements.define('header-nav', HeaderNav);
+customElements.define('header-nav', HeaderNav);
+
+// INTRO SECTIION COMPONENT
+const introSectionTemplate = document.createElement('template');
+introSectionTemplate.innerHTML = `
+        <style>
+            .intro-section {
+                display: flex;
+                justify-content: flex-start;
+                align-items: top;
+                height: 100vh;
+                background-color: #f1f1f1;
+                color: #000;
+                padding: 2rem;
+            }
+
+            h1 {
+                margin: 0;
+            }
+        </style>
+        <section class="intro-section">
+            <h1>Intro Section</h1>
+        </section>
+    `;
+
+class IntroSection extends HTMLElement {
+    constructor() {
+        super();
+        const shadow = this.attachShadow({ mode: 'open' });
+        shadow.appendChild(introSectionTemplate.content.cloneNode(true));
+    }
+}
+
+customElements.define('intro-section', IntroSection);
