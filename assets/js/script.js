241cd080cb64719eb100707873e24de99f5103b1
fullStackHero <hello@avui.design>
2024-09-28 17:01:29 +0000

    init commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+class TodoItem extends HTMLElement {
+    constructor() {
+        super();
+        this.innerHTML = `<h3>${this.innerText}</h3>`;
+    }
+}
+
+customElements.define('todo-item', TodoItem);
